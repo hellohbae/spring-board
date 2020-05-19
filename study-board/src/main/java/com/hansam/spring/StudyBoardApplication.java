e709af2801aa82e1e25706662cc2a1d9d6f65894
Ubuntu <ubuntu@ip-172-31-27-94.ap-northeast-2.compute.internal>
2020-05-19 21:53:49 +0000

    add SpringBootServletInitializer for deploy

--- study-board/src/main/java/com/hansam/spring/StudyBoardApplication.java
+++ study-board/src/main/java/com/hansam/spring/StudyBoardApplication.java
@@ -9,22 +9,24 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
 @SpringBootApplication
 @MapperScan(value= {"com.hansam.spring.mapper"})
-public class StudyBoardApplication {
+public class StudyBoardApplication extends SpringBootServletInitializer{
 
 	public static void main(String[] args) {
 		SpringApplication.run(StudyBoardApplication.class, args);
 	}
-	
+
 	@Bean
-    public SqlSessionFactory sqlSessionFactory(DataSource dataSource) throws Exception{
-        
-        SqlSessionFactoryBean sessionFactory = new SqlSessionFactoryBean();
-        
-        sessionFactory.setDataSource(dataSource);
-        return sessionFactory.getObject();
-        
-    }
+	public SqlSessionFactory sqlSessionFactory(DataSource dataSource) throws Exception{
+
+		SqlSessionFactoryBean sessionFactory = new SqlSessionFactoryBean();
+
+		sessionFactory.setDataSource(dataSource);
+		return sessionFactory.getObject();
+
+	}
 
 }
