9d321e2f069be3a3bfdb1f0fe140cc8febe5327a
user-PC\user <wert7871@gmail.com>
2020-05-20 08:29:10 +0900

    simple board complete

--- study-board/src/main/java/com/hansam/spring/controller/BoardController.java
+++ study-board/src/main/java/com/hansam/spring/controller/BoardController.java
@@ -7,6 +7,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 
 import com.hansam.spring.domain.BoardVO;
 import com.hansam.spring.service.BoardService;
@@ -17,53 +18,68 @@ public class BoardController {
 	@Autowired
 	BoardService mBoardService;
 	
+	@GetMapping("/test")
+	public String testview() throws Exception {
+
+		return "test2";
+	}
+
+
 	@GetMapping("/list")
-	public String boardList(Model model) throws Exception{
+	public String boardList(Model model) throws Exception {
 		model.addAttribute("list", mBoardService.boardListService());
-		
+
 		return "list";
 	}
-	
+
 	@GetMapping("/detail/{bno}")
-	public String boardDetail(@PathVariable int bno, Model model) throws Exception{
+	public String boardDetail(@PathVariable int bno, Model model) throws Exception {
 		model.addAttribute("detail", mBoardService.boardDetailService(bno));
-		
+
 		return "detail";
 	}
-	
+
 	@GetMapping("/insert")
-	public String boardInsertForm(){
+	public String boardInsertForm() {
 		return "insert";
 	}
-	
-	@GetMapping("/insertProc")
-	public String boardInsertProc(HttpServletRequest request) throws Exception{
-		BoardVO board = (BoardVO) request.getParameterMap();
-		
+
+	@PostMapping("/insertProc")
+	public String boardInsertProc(HttpServletRequest request) throws Exception {
+		BoardVO board = new BoardVO();
+
+		board.setSubject(request.getParameter("subject"));
+		board.setContent(request.getParameter("content"));
+		board.setWriter(request.getParameter("writer"));
+
 		mBoardService.boardInsertService(board);
-		
+
 		return "redirect:/list";
 	}
-	
+
 	@GetMapping("/update/{bno}")
-	public String boardUpdateForm(@PathVariable int bno, Model model) throws Exception{
+	public String boardUpdateForm(@PathVariable int bno, Model model) throws Exception {
 		model.addAttribute("detail", mBoardService.boardDetailService(bno));
-		
+
 		return "update";
 	}
-	
-	@GetMapping("/updateProc")
-	public String boardUpdateProc(HttpServletRequest request) throws Exception{
-		BoardVO board = (BoardVO)request.getParameterMap();
+
+	@PostMapping("/updateProc")
+	public String boardUpdateProc(HttpServletRequest request) throws Exception {
+		BoardVO board = new BoardVO();
+		board.setSubject(request.getParameter("subject"));
+		board.setContent(request.getParameter("content"));
+		board.setBno(Integer.parseInt(request.getParameter("bno")));
+
 		mBoardService.boardUpdateService(board);
-		
-		return "redirect://detail/"+board.getBno();
+
+		return "redirect:/detail/" + request.getParameter("bno");
 	}
-	
+
 	@GetMapping("/delete/{bno}")
-	public String boardDelete(@PathVariable int bno) throws Exception{
+	public String boardDelete(@PathVariable int bno) throws Exception {
 		mBoardService.boardDeleteService(bno);
-		
+
 		return "redirect:/list";
 	}
 }
